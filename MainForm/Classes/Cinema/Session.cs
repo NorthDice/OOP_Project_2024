1e4edb563f5537333356bbdd56a742a953271192
Balychev Kiril <mister.balychev@gmail.com>
2024-03-29 19:07:09 +0200

    Update get set function; Add restrictions; Update code;

--- MainForm/Classes/Cinema/Session.cs
+++ MainForm/Classes/Cinema/Session.cs
@@ -37,40 +37,78 @@ namespace MainForm.Classes.Cinema
 
         public bool ReturnTicket(DateTime currentDateTime)
         {
-            throw new NotImplementedException();
+            
+            TimeSpan timeRemaining = Date - currentDateTime;
+
+            if (timeRemaining.TotalHours < 6)
+            {
+                
+                return false;
+            }
+            
+            return true;
         }
+
         public void ViewSessionInformation()
         {
-            throw new NotImplementedException(); 
+            Console.WriteLine($"Film name: {FilmName}\n Session date: {Date}\n Session hall {HallNumber}");
         }
 
         public void DisplayDescription(string filmName, string director, int year, string description)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Film name: {filmName}\n Film Director: {director}\n Issue date: {year}\n Short description: {description}");
         }
 
         public DateTime Date
         {
             get { return _date; }
-            set { _date = value; }
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new ArgumentOutOfRangeException("Date cannot be in the past");
+                }
+            }
         }
 
         public TimeSpan Time
         {
             get { return _time; }
-            set { _time = value; }
+            set
+            {
+                if (value < DateTime.Now.TimeOfDay)
+                {
+                    throw new ArgumentOutOfRangeException("Time cannot be in the past");
+                }
+
+                _time = value;
+            }
         }
 
         public Halls HallNumber
         {
             get { return _hallNumber; }
-            set { _hallNumber = value; }
+            set
+            {
+                if (!Enum.IsDefined(typeof(Halls), value))
+                {
+                    throw new ArgumentException("Invalid value for room number!");
+                }
+                _hallNumber = value;
+            }
         }
 
         public string FilmName
         {
             get { return _filmName; }
-            set { _filmName = value; }
+            set 
+            { 
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Film name can not be null or empty!");
+                }
+                _filmName = value;
+            }
         }
     }
 }
